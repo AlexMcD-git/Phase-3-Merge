090f10a7be1d667c7eef350128c9620290758b9c
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-08-29 02:31:50 -0400

    copied old files over

--- app/models/hero.rb
+++
@@ -1,5 +0,0 @@
-class Hero < ActiveRecord::Base
-    has_many :quests
-    has_many :enemies, through: :quests
-    has_many :items
-end
--- app/models/quest.rb
+++
@@ -1,4 +0,0 @@
-class Quest < ActiveRecord::Base
-    belongs_to :enemy
-    belongs_to :hero
-end
